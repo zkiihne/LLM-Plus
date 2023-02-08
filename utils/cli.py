8f5d6382b54096402b55446c4eec99f546260cc2
Zachary Kiihne <zkiihne@gmail.com>
2023-02-08 11:25:27 -0500

    Create cli.py

---
+++ utils/cli.py
@@ -0,0 +1,11 @@
+# cli for creating and using queries
+
+# options for command:
+# llm-plus
+# --LLM [ChatGPT] 
+# --context [string:string]
+# --data_source [pokimon, todolist]
+# --response_format [json, all_caps]
+# --input_file [filename]
+
+# a preview will be shown, then you will be asked to input your prompt text
