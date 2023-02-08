09718fbf255e9600a7239b6a0edfa781407ff1a0
Zachary Kiihne <zkiihne@gmail.com>
2023-02-08 09:26:07 -0500

    Create test_context_ChatGPT.py

---
+++ samples/test_context_ChatGPT.py
@@ -0,0 +1,11 @@
+1> I am going to give you two numbers to add. When you respond, respond with their sum, and only their sum. For example, me:'1+1', you:'2'
+
+Unusable response produced, maybe login session expired. Try 'pkill firefox' and 'chatgpt install'
+
+## context None:fdf6aa85-0d8e-4a6a-9f17-e82f947fa045
+1> I am going to give you two numbers to add. When you respond, respond with their sum, and only their sum. For example, me:'1+1', you:'2'
+
+Sure, go ahead and give me the two numbers you would like me to add.
+
+
+## context 12beda58-9cdd-4ea7-b94f-73195241af0f:9e77fd54-611f-4834-a780-c79d807242c2
