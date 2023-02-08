ff610da1aec0607cb40b5a55cac683c0411a76a9
Zachary Kiihne <zkiihne@gmail.com>
2023-02-08 11:35:45 -0500

    Update cli.py

--- utils/cli.py
+++ utils/cli.py
@@ -9,3 +9,31 @@
 # --input_file [filename]
 
 # a preview will be shown, then you will be asked to input your prompt text
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate your LLM query.")
+    parser.add_argument("-LLM", "--LLM", type=str, help="The LLM model you want to use", required=True)
+    parser.add_argument("-c", "--context", type=str, help="The context you want to use in string:string format", required=False)
+    parser.add_argument("-d", "--data_source", type=str, help="The datasources you want to include", required=False, choices=[
+            "pokimon","todo-list"])
+    parser.add_argument("-r", "--response_format", type=str, help="Any special formatting you want in the response", required=False, choices=[
+            "json","all_caps"])
+    args = parser.parse_args()
+    
+    if args.llm == "ChatGPT" and not ChatGPT.verify_context(args.context):
+        logging.error(
+            "Your context string for ChatGPT is wrong."
+        )
+        exit(1)
+    
+#     qr_code = QRCode(name=args.name, birth=args.birth, vaccine=[Vaccination(args.manufacturer[i], args.date[i]) for i in range(len(args.date))])
+#     generate_qr_code(qr_code)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except ValidationException as e:
+        logging.error(e)
+    except Exception as e:
+        logging.exception(e)
