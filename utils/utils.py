c5e51f1fdd61a0c638d1de07a74b516a046b00e5
Zachary Kiihne <zkiihne@gmail.com>
2023-02-09 10:03:57 -0500

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,27 @@
 class Util(object):
   
-  def __init__(self, llm="ChatGPT", ):
+  def __init__(self, llm="ChatGPT", response_format="", data_source="", context=None):
     self.llm = llm
+    self.response_format = response_format
+    self.data_source = data_source
+    self.context = context
     
     self.user_input = "[ user input here ]"
-      
+    
   def set_user_input(self, user_input):
     self.user_input = user_input
     
   def preview(self):
-    print(construct_query)
+    print("Using LLM: " + llm)
+    print("With context: " + context)
+    print(construct_query())
     
   def construct_query(self):
     # constructs the query to be sent
+    query = self.intro
+    query += "\n" + self.get_data_source(data_source)
+    query += "\n" + self.response_format
+    query += "\n" + self.user_input
+    
+  def add_context(self):
+    # add context to model
