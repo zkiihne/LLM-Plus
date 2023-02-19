e65c36fba57732b2f6f952ef25221a2e40c25c79
Zachary Kiihne <zkiihne@gmail.com>
2023-02-19 16:31:51 -0500

    Update graphqlbackend.py

--- graphqlbackend.py
+++ graphqlbackend.py
@@ -9,7 +9,7 @@ class Response(ObjectType):
 class Query(ObjectType):
     query = List(Response, user_input=String(), model=String(), context=String())
     
-    def resolve_query(self, model, context, user_input):
+    def resolve_query(self, dummy, model, context, user_input):
         llm_util = Util(llm=model, context=context, user_input=user_input)
         response = llm_util.send_query()
         return_dict = {}
