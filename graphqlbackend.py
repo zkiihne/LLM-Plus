bd8e94ad11331ba8257b1f00fd579d0381710a2e
Zachary Kiihne <zkiihne@gmail.com>
2023-02-19 16:23:52 -0500

    Update graphqlbackend.py

--- graphqlbackend.py
+++ graphqlbackend.py
@@ -1,10 +1,19 @@
 from graphene import ObjectType, String, Boolean, ID, Field, Int,List , Float
 from utils import Util
 
+class Response(ObjectType):
+    query = String()
+    response = String()
+    model = String()
+    
 class Query(ObjectType):
-    query = List(user_input=String(), model=String(), context=String())
+    query = List(Response, user_input=String(), model=String(), context=String())
     
     def resolve_query(self, model, context, user_input):
         llm_util = Util(llm=model, context=context, user_input=user_input)
         response = llm_util.send_query()
-        return [response]
+        return_dict = {}
+        return_dict.update({"response":response})
+        return_dict.update({"query":query})
+        return_dict.update({"model":model})
+        return [return_dict]
